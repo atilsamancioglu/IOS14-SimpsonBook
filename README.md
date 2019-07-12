0ddcec9b58965179e4620b359a5f6b155d329359
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-13 00:03:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# IOS14-SimpsonBook
---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,9 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Atil Samancioglu on 11.07.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import Foundation
